ef9630b468c68777eb4cb2e22b6d595b5dd6f490
Aniruddha-Deb <aniruddha.deb.2002@gmail.com>
2020-07-06 14:20:59 +0530

    Moving to a flask server

--- controller.js
+++ controller.js
@@ -1,21 +1,7 @@
-var imgLoader = null;
-const K = 20;
-const dbName = "facemash_db"
-const guytable = "guy_table"
-const galtable = "gal_table"
+var apiLoc = "/api/gals/";
 
-const guyimgdir = "img/guys"
-const galimgdir = "img/gals"
-
-const guyStartIndex = 0;
-const guyEndIndex = 3;
-const galStartIndex = 0;
-const galEndIndex = 3;
-
-var db;
-
-var leftImage;
-var rightImage;
+var leftImageId;
+var rightImageId;
 
 function onLikeGuysClicked() {
 	sessionStorage.setItem("prefGuys", true);
@@ -28,94 +14,29 @@ function onLikeGalsClicked() {
 }
 
 function onFacemashLoad() {
-	// check if prev DB exists; if not, create a new one and init all
-	// ELO to 1200.
-
-	var request = indexedDB.open(dbName);
-	request.onupgradeneeded = function(e) { initDB(e) };
-	request.onerror = function(e) {console.log("Cannot use local DB");}
-	request.onsuccess = function(e) {db = e.target.result;}
-
 	if (sessionStorage.getItem("prefGuys") === "true") {
-		imgLoader = new ImageLoader(guyimgdir, guyStartIndex, guyEndIndex, "jpg");
-	}
-	else {
-		imgLoader = new ImageLoader(galimgdir, galStartIndex, galEndIndex, "jpg");
+		apiLoc = "/api/guys/";
 	}
 	onNextClicked();
 }
 
-function initDB(event) {
-	db = event.target.result;
+function onImageClicked(side) {
+	var apiUrl = apiLoc + "click?leftid=" + encodeURIComponent(leftImageId) + "&rightid=" +
+	encodeURIComponent(rightImageId) + "&side=" + encodeURIComponent(side);
 
-	if (!db.objectStoreNames.contains(guytable)) {
-		var guyObjStore = db.createObjectStore(guytable);
-		for (var i=guyStartIndex; i<=guyEndIndex; i++) {
-			guyObjStore.add(1200, i);
-		}
-	}
-	if (!db.objectStoreNames.contains(galtable)) {
-		var galObjStore = db.createObjectStore(galtable);
-		for (var i=galStartIndex; i<=galEndIndex; i++) {
-			galObjStore.add(1200, i);
-		}
-	}	
-	console.log("Inited DB");
-}
-
-function updateElo(table, winnerId, loserId) {
-	var objStore = db.transaction([table], "readwrite").objectStore(table);
-
-	// JS is a humongous piece of shit for this annoying async db 
-	// callback thing....
-	var winnerInitialEloRequest = objStore.get(winnerId);
-	winnerInitialEloRequest.onsuccess = function(e) {
-		var loserInitialEloRequest = objStore.get(loserId);
-		loserInitialEloRequest.onsuccess = function(f) {
-			var winnerInitialElo = winnerInitialEloRequest.result;
-			var loserInitialElo = loserInitialEloRequest.result;
-
-			var winnerR = Math.pow(10, winnerInitialElo/400);
-			var loserR = Math.pow(10, loserInitialElo/400);
-
-			var winnerFactor = 1 - (winnerR/(winnerR + loserR));
-			var loserFactor = 0 - (loserR/(winnerR + loserR));
-
-			var winnerFinalElo = winnerInitialElo + K*(winnerFactor);
-			var loserFinalElo = loserInitialElo + K*(loserFactor);
-
-			console.log(winnerId + ": " + winnerInitialElo + " -> " + winnerFinalElo); 
-			console.log(loserId + ": " + loserInitialElo + " -> " + loserFinalElo); 
-
-			objStore.put(winnerFinalElo, winnerId);
-			objStore.put(loserFinalElo, loserId);
-		}
-	}
+	fetch(apiUrl).then(
+    	function(response) {
+			onNextClicked();
+    	}
+  	);
 }
 
 function onLeftImageClicked() {
-	console.log("Left img clicked");
-	if (sessionStorage.getItem("prefGuys") === "true") {
-		// Again, JS is a humongous piece of shit for implicit
-		// type declaration, you don't know what type you're 
-		// passing to the function....
-		updateElo(guytable, parseInt(leftImage.id), parseInt(rightImage.id));
-	}
-	else {
-		updateElo(galtable, parseInt(leftImage.id), parseInt(rightImage.id));
-	}
-	onNextClicked();
+	onImageClicked("left");
 }
 
 function onRightImageClicked() {
-	console.log("Right img clicked");
-	if (sessionStorage.getItem("prefGuys") === "true") {
-		updateElo(guytable, parseInt(rightImage.id), parseInt(leftImage.id));
-	}
-	else {
-		updateElo(galtable, parseInt(rightImage.id), parseInt(leftImage.id));
-	}
-	onNextClicked();
+	onImageClicked("right");
 }
 
 function onStopClicked() {
@@ -129,76 +50,31 @@ function onLeaderboardClicked() {
 }
 
 function onLeaderboardLoaded() {
-
-	var request = indexedDB.open(dbName);
-	request.onsuccess = function(e) {db = e.target.result;
-	var table = document.getElementById("leaderboard");
-
-	console.log("Got leaderboard");
-	if (sessionStorage.getItem("prefGuys") === "true") {
-		var objStore = db.transaction([guytable], "readwrite").objectStore(guytable);
-		objStore.openCursor().onsuccess = function(e) {
-		console.log("Txn success");
-			var cursor = e.target.result;
-			if (cursor) {
-				let row = table.insertRow();
-				let id = row.insertCell(0);
-				id.innerHTML = cursor.key;
-				let img = row.insertCell(1);
-				img.innerHTML = 
-				"<img class=\"thumbnail\" src=\""+guyimgdir+"/"+cursor.key+".jpg\">";
-				let elo = row.insertCell(2);
-				elo.innerHTML = Math.floor(cursor.value);
-				cursor.continue();
-			}
-		}
-	}
-	else {
-		var objStore = db.transaction([galtable], "readwrite").objectStore(galtable);
-		objStore.openCursor().onsuccess = function(e) {
-		console.log("Txn success");
-			var cursor = e.target.result;
-			if (cursor) {
-				let row = table.insertRow();
-				let id = row.insertCell(0);
-				id.innerHTML = cursor.key;
-				let img = row.insertCell(1);
-				img.innerHTML = 
-				"<img class=\"thumbnail\" src=\""+galimgdir+"/"+cursor.key+".jpg\">";
-				let elo = row.insertCell(2);
-				elo.innerHTML = Math.floor(cursor.value);
-				cursor.continue();
-			}
-		}
-	}	
-}
+	// TODO this......
 }
 
 function onNextClicked() {
 	console.log("Next image clicked");
-	leftImage = imgLoader.loadRandomImage();
-	rightImage = imgLoader.loadRandomImage();
-	while (leftImage.id == rightImage.id) {
-		rightImage = imgLoader.loadRandomImage();
-	}
-	document.getElementById("leftImage").src = leftImage.src;
-	document.getElementById("rightImage").src = rightImage.src;
+
+	var apiUrl = apiLoc+"getrandomimg";
+
+	fetch(apiUrl).then(
+    	function(response) {
+    		response.text().then( (r) => {
+    			console.log(r);
+				document.getElementById("leftImage").src = apiLoc+r;
+				leftImageId = r.split(".")[0];
+    		});
+    	}
+  	)
+
+	fetch(apiUrl).then(
+    	function(response) {
+    		response.text().then( (r) => {
+    			console.log(r);
+				document.getElementById("rightImage").src = apiLoc+r;
+				rightImageId = r.split(".")[0];
+    		});
+    	}
+  	)
 }
-
-class ImageLoader {
-	constructor(dir, is, ie, itype) {
-		this.startindex = is;
-		this.endindex = ie;
-		this.directory = dir;
-		this.imgType = itype
-	}
-
-	loadRandomImage() {
-		var imgId = Math.floor(Math.random() * (this.endindex - this.startindex + 1)) + this.startindex;
-		var img = new Image();
-		img.src = this.directory+"/"+imgId+"."+this.imgType;
-		img.id = imgId;
-		return img;
-	}
-}
-
---
+++ facemash.py
@@ -0,0 +1,133 @@
+from flask import Flask, request, send_file, send_from_directory
+import os
+import sqlite3
+import atexit
+from random import randint
+# set the project root directory as the static folder, you can set others.
+app = Flask(__name__, static_folder='')
+
+K = 40
+
+GUYS_DIR = "imgdup/guys"
+GALS_DIR = "imgdup/gals"
+DB_LOC = "db/data.db"
+
+GUYS_TABLE = "GUYS_TABLE"
+GALS_TABLE = "GALS_TABLE"
+KEY_ID = "id"
+KEY_ELO = "elo"
+
+@app.before_first_request
+def run_on_start():
+	db_conn = sqlite3.connect(DB_LOC)
+	db_cursor = db_conn.cursor()
+	initDB(db_conn, db_cursor)
+
+def initDB(db_conn, db_cursor):
+	db_cursor.execute("CREATE TABLE IF NOT EXISTS " + GUYS_TABLE + " (" 
+		+ KEY_ID + " INTEGER PRIMARY KEY,"
+		+ KEY_ELO + " INTEGER DEFAULT 1200)" )
+	db_cursor.execute("CREATE TABLE IF NOT EXISTS " + GALS_TABLE + " (" 
+		+ KEY_ID + " INTEGER PRIMARY KEY,"
+		+ KEY_ELO + " INTEGER DEFAULT 1200)" )
+	db_conn.commit()
+
+@app.route("/api/guys/<img>")
+def send_guy_img(img):
+    return send_from_directory(GUYS_DIR, img)
+
+@app.route("/api/gals/<img>")
+def send_gal_img(img):
+    return send_from_directory(GALS_DIR, img)
+
+
+@app.route('/')
+@app.route('/index')
+def root():
+    return app.send_static_file('facemash_intro_page.html')
+
+@app.route('/api/guys/getrandomimg')
+def get_random_guy_img():
+	return get_random_img(GUYS_DIR)
+
+@app.route('/api/gals/getrandomimg')
+def get_random_gal_img():
+	return get_random_img(GALS_DIR)
+
+def get_random_img(dir):
+	# TODO not return the same image twice...
+	imgList = os.listdir(dir)
+	index = randint(0,len(imgList)-1)
+	# while index == prev_img:
+	# 	index = randint(0,len(imgList)-1)
+	# prev_img = index;
+	return imgList[index]
+
+@app.route('/api/gals/click')
+def gal_click():
+	leftid = request.args.get("leftid")
+	rightid = request.args.get("rightid")
+	side = request.args.get("side")
+	if side == "left":
+		return update_elo(GALS_TABLE, leftid, rightid)
+	elif side == "right":
+		return update_elo(GALS_TABLE, rightid, leftid)
+	return "Could not update ELO"
+
+@app.route('/api/guys/click')
+def guy_click():
+	leftid = request.args.get("leftid")
+	rightid = request.args.get("rightid")
+	side = request.args.get("side")
+	if side == "left":
+		return update_elo(GUYS_TABLE, leftid, rightid)
+	elif side == "right":
+		return update_elo(GUYS_TABLE, rightid, leftid)
+	return "Could not update ELO"
+
+def update_elo(table, winnerid, loserid):
+	print("Updating ELO for " + str(winnerid) + "," + str(loserid));
+	db_conn = sqlite3.connect(DB_LOC)
+	db_cursor = db_conn.cursor()
+
+	entry_exists_query = "SELECT EXISTS(SELECT 1 FROM " + table + " WHERE " + KEY_ID + "=?);"
+	new_entry_query = "INSERT INTO "+table+"("+KEY_ID+") VALUES (?)"
+	get_entry_query = "SELECT * FROM "+table+" WHERE "+KEY_ID+"=?"
+	update_entry_query = "UPDATE "+table+" SET "+KEY_ELO+"=? WHERE "+KEY_ID+"=?"
+
+	# insert entry if not exists
+	winner_exists = db_cursor.execute(entry_exists_query, (winnerid,)).fetchone()[0]
+	loser_exists = db_cursor.execute(entry_exists_query, (loserid,)).fetchone()[0]
+
+	if winner_exists == 0:
+		print("Winner not exists; making entry")
+		db_cursor.execute(new_entry_query, (winnerid,))
+	if loser_exists == 0:
+		print("Loser not exists; making entry")
+		db_cursor.execute(new_entry_query, (loserid,))
+
+	db_conn.commit();
+
+	print(db_cursor.execute(get_entry_query, (winnerid,)).fetchone());
+	print(db_cursor.execute(get_entry_query, (loserid,)).fetchone());
+
+	winner_initial_elo = db_cursor.execute(get_entry_query, (winnerid,)).fetchone()[1];
+	loser_initial_elo = db_cursor.execute(get_entry_query, (loserid,)).fetchone()[1];
+
+	winner_R = 10 ** (winner_initial_elo/400)
+	loser_R = 10 ** (loser_initial_elo/400)
+
+	winner_factor = 1 - winner_R/(winner_R+loser_R)
+	loser_factor = 0 - loser_R/(loser_R+winner_R)
+
+	winner_final_elo = winner_initial_elo + K*winner_factor;
+	loser_final_elo = loser_initial_elo + K*loser_factor;
+
+	db_cursor.execute(update_entry_query, (winner_final_elo,winnerid))
+	db_cursor.execute(update_entry_query, (loser_final_elo,loserid))
+
+	print("winner: " + str(winner_initial_elo) + " -> " + str(winner_final_elo));
+	print("loser: " + str(loser_initial_elo) + " -> " + str(loser_final_elo));
+	db_conn.commit();
+	db_conn.close();
+	return "good";
